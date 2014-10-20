0ab7e5caed8a800507709b8d5612d6f24e47434f
kentsurrey <luke.dennis@cantab.net>
2014-10-20 12:22:12 +0100

    Update StatsdListener.java

--- src/main/java/org/jenkinsci/plugins/statsd/StatsdListener.java
+++ src/main/java/org/jenkinsci/plugins/statsd/StatsdListener.java
@@ -1,19 +1,14 @@
 package org.jenkinsci.plugins.statsd;
 
-import hudson.model.Run;
-import hudson.Extension;
-import hudson.model.*;
-import hudson.model.listeners.RunListener;
-import jenkins.model.GlobalConfiguration;
-import net.sf.json.JSONObject;
-import org.kohsuke.stapler.StaplerRequest;
-
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static hudson.model.Result.*;
+import hudson.Extension;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.model.listeners.RunListener;
 
 /**
  * Send jenkins result and duration of Jenkins jobs to a statsd server
@@ -21,7 +16,8 @@ import static hudson.model.Result.*;
 @Extension
 public class StatsdListener extends RunListener<Run> {
 
-    private static final Logger LOGGER = Logger.getLogger(StatsdListener.class.getName());
+	private static final char METRIC_SEPARATOR = '.';
+	private static final Logger LOGGER = Logger.getLogger(StatsdListener.class.getName());
 
     @Override
     public void onCompleted(final Run r, final TaskListener listener) {
@@ -32,21 +28,15 @@ public class StatsdListener extends RunListener<Run> {
             return;
         }
 
-        String prefix = config.getPrefix();
         String host = config.getHost();
         int port = config.getPort();
 
         String jobName = r.getParent().getFullName().toString();
         String result = r.getResult().toString();
         long duration = r.getDuration();
+		jobName = sanitizeName(jobName);
 
-        // sanitize jobName for statsd/graphite. based on: https://github.com/etsy/statsd/blob/v0.5.0/stats.js#L110-113
-        jobName = jobName.replaceAll("\\s+", "_");
-        jobName = jobName.replaceAll("\\.", "_");
-        jobName = jobName.replaceAll("\\/", "-");
-        jobName = jobName.replaceAll("[^a-zA-Z_\\-0-9]", "");
-
-        String metricName = prefix + '.' + jobName + '.' + result;
+		String metricName = generateMetricName(r, config, jobName, result);
 
         LOGGER.log(Level.INFO, "StatsdListener: config: " + config);
         LOGGER.log(Level.INFO, "StatsdListener: job: " + jobName + ", result: " + result +
@@ -63,4 +53,34 @@ public class StatsdListener extends RunListener<Run> {
         }
     }
 
+	/**
+	 * @return The display name of the node that the job was run on.
+	 */
+	private String getNodeName(Run run) {
+		return run.getExecutor().getOwner().getNode().getDisplayName();
+	}
+
+	/**
+	 * @return The metric name. A concatenation of the configured prefix, the job name, the
+	 * node's display name (only if configured to do so) and finally the result.
+	 */
+	private String generateMetricName(Run run, StatsdConfig config, String jobName, String result) {
+		String metricName = config.getPrefix() + METRIC_SEPARATOR + jobName;
+		if (config.isIncludeNodeInName()) {
+			String nodeName = getNodeName(run);
+			metricName = metricName + METRIC_SEPARATOR + sanitizeName(nodeName);
+		}
+		return metricName + METRIC_SEPARATOR + result;
+	}
+
+	/**
+	 * Sanitize name for statsd/graphite. based on: https://github.com/etsy/statsd/blob/v0.5.0/stats.js#L110-113
+	 */
+	private String sanitizeName(String name) {
+		name = name.replaceAll("\\s+", "_");
+		name = name.replaceAll("\\.", "_");
+		name = name.replaceAll("\\/", "-");
+		name = name.replaceAll("[^a-zA-Z_\\-0-9]", "");
+		return name;
+	}
 }
